fc3ef743681191ac6278f7876adb81a93ecbf5f3
Nickolausen <nicholas.magi24@gmail.com>
2024-11-14 22:45:41 +0100

    added lab04 - hippi hip i

---
+++ lab04/41-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
@@ -0,0 +1,10 @@
+package it.unibo.inheritance.impl;
+
+public class ExtendedStrictBankAccount extends SimpleBankAccount {
+
+    public ExtendedStrictBankAccount(int id, double balance) {
+        super(id, balance);
+    }
+    
+    
+}
