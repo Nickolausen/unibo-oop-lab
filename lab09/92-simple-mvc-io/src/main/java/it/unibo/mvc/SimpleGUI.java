7c875950a95f9994c80df1a223258849190cccf2
Nickolausen <nicholas.magi24@gmail.com>
2024-11-19 23:38:10 +0100

    fixed mess

---
+++ lab09/92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -0,0 +1,79 @@
+package it.unibo.mvc;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+/**
+ * A very simple program using a graphical interface.
+ * 
+ */
+public final class SimpleGUI {
+
+        private static final String TITLE = "My first java graphical interface";
+        private static final int PROPORTION = 5;
+        private final JFrame frame = new JFrame(TITLE);
+        private final Controller controller;
+
+        /**
+         * Default constructor.
+         */
+        public SimpleGUI() {
+            this.controller = new Controller();
+            setupView();
+        }
+
+        /**
+         * Renders the main frame.
+         */
+        public void display() {
+            final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+            final int sw = (int) screen.getWidth();
+            final int sh = (int) screen.getHeight();
+            frame.setSize(sw / PROPORTION, sh / PROPORTION);
+            frame.setLocationByPlatform(true);
+            frame.setVisible(true);
+        }
+
+        /**
+         * Initializes the elements in the view.
+         */
+        private void setupView() {
+            final JPanel canvas = new JPanel();
+            canvas.setLayout(new BorderLayout());
+            final JTextArea textArea = new JTextArea();
+            canvas.add(textArea);
+            final JButton save = new JButton("Save");
+            save.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(final ActionEvent e) {
+                    try {
+                        controller.writeContent(textArea.getText());
+                    } catch (final IOException ex) {
+                        JOptionPane.showMessageDialog(frame, ex, "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            });
+            canvas.add(save, BorderLayout.SOUTH);
+            this.frame.add(canvas);
+        }
+
+
+    /**
+     * Runs the application.
+     * 
+     * @param args ignored
+     */
+    public static void main(final String... args) {
+        new SimpleGUI().display();
+    }
+}
