cefcdb15b983a863e9b5a3ab77bca8fce4f6a5b2
Nickolausen <nicholas.magi24@gmail.com>
2024-12-04 17:21:57 +0100

    finished lab11

--- lab11/114-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
+++ lab11/114-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
@@ -2,39 +2,100 @@ package it.unibo.oop.workers02;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * Multithreaded matrix sum.
+ */
 public class MultiThreadedSumMatrix implements SumMatrix {
 
     private final int nThreads;
-    
+    /**
+     * 
+     * @param nThreads the number of threads used
+     */
     public MultiThreadedSumMatrix(final int nThreads) {
         this.nThreads = nThreads;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public double sum(double[][] matrix) {
-        final List<Worker> workers = new ArrayList<Worker>(this.nThreads);
-        workers.forEach(worker -> {
-            
-        });
-        return 0.0;
+    public double sum(final double[][] matrix) {
+        final int size = matrix.length % nThreads + matrix.length / nThreads;
+        /*
+         * Build a list of workers
+         */
+        final List<Worker> workers = new ArrayList<>(nThreads);
+        for (int start = 0; start < matrix.length; start += size) {
+            workers.add(new Worker(matrix, start, size));
+        }
+        /*
+         * Start them
+         */
+        for (final Worker w: workers) {
+            w.start();
+        }
+        /*
+         * Wait for every one of them to finish. This operation is _way_ better done by
+         * using barriers and latches, and the whole operation would be better done with
+         * futures.
+         */
+        double sum = 0;
+        for (final Worker w: workers) {
+            try {
+                w.join();
+                sum += w.getResult();
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+        }
+        /*
+         * Return the sum
+         */
+        return sum;
     }
 
     private static class Worker extends Thread {
 
+        private final double[][] matrixSlice;
+        private final int startPos;
+        private final int nElem;
+        private double res;
+
         /**
          * Build a new worker.
          * 
-         * @param list
-         *            the list to sum
-         * @param startpos
+         * @param matrixSlice
+         *            the slice of matrix to sum
+         * @param startPos
          *            the initial position for this worker
-         * @param nelem
+         * @param nElem
          *            the no. of elems to sum up for this worker
          */
-        Worker(final List<Integer> list, final int startpos, final int nelem) {
+        Worker(final double[][] matrixSlice, final int startPos, final int nElem) {
             super();
-            
+            this.startPos = startPos;
+            this.nElem = nElem;
+            this.matrixSlice = matrixSlice.clone();
+        }
+
+        /**
+         * Returns the result of summing up the integers within the list.
+         * 
+         * @return the sum of every element in the array
+         */
+        public double getResult() {
+            return this.res;
+        }
+
+        @Override
+        @SuppressWarnings("PMD.SystemPrintln")
+        public void run() {
+            for (int i = startPos; i < matrixSlice.length && i < startPos + nElem; i++) {
+                for (final var val: matrixSlice[i]) {
+                    this.res += val;
+                }
+            }
         }
     }
 }
